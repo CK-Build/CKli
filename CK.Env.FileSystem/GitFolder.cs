b8eef7bd786863b488f1a6363735f56f25eae7b8
Benjamin Crosnier <benjamin.crosnier@signature-code.com>
2018-03-09 19:29:37 +0100

    Move static ObtainGitCredentialsHandler(m) from XGitFolder to GitFolder

--- CK.Env.FileSystem/GitFolder.cs
+++ CK.Env.FileSystem/GitFolder.cs
@@ -533,5 +533,54 @@ namespace CK.Env
             return false;
         }
 
+        public static CredentialsHandler ObtainGitCredentialsHandler( IActivityMonitor m )
+        {
+            return ( url, usernameFromUrl, types ) =>
+            {
+                string domain = new Uri( url ).Host;
+                string usernameEnvironmentVariableKey = $"GIT_USER_{domain}";
+                string passwordEnvironmentVariableKey = $"GIT_PWD_{domain}";
+
+                m.Info( $"Looking for credentials for domain {domain} in environment: {usernameEnvironmentVariableKey}, {passwordEnvironmentVariableKey}, and URL" );
+
+                // Guess username: use URL, fallback on env. var
+                string username = usernameFromUrl;
+                if( string.IsNullOrEmpty( username ) )
+                {
+                    username = Environment.GetEnvironmentVariable( usernameEnvironmentVariableKey );
+
+                    if( string.IsNullOrEmpty( username ) )
+                    {
+                        m.Warn( $"Git username was not found at environment variable {usernameEnvironmentVariableKey}. Git might fail if the repository requires authentication." );
+                    }
+                    else
+                    {
+                        m.Info( $"Using Git username from environment variable ({usernameEnvironmentVariableKey}): {username}" );
+                    }
+                }
+                else
+                {
+                    m.Info( $"Using Git username from URL: {username}" );
+                }
+
+                // Read password from env. var
+
+                if( !string.IsNullOrEmpty( username ) )
+                {
+                    string password = Environment.GetEnvironmentVariable( passwordEnvironmentVariableKey );
+
+                    if( string.IsNullOrEmpty( password ) )
+                    {
+                        m.Warn( $"Git password was not found at environment variable {passwordEnvironmentVariableKey}. Git might fail if the repository requires authentication." );
+                    }
+                    else
+                    {
+                        m.Info( $"Using Git password from environment variable ({passwordEnvironmentVariableKey})" );
+                        return new UsernamePasswordCredentials() { Username = username, Password = password };
+                    }
+                }
+                return new DefaultCredentials();
+            };
+        }
     }
 }
--- CKli/XGitFolder.cs
+++ CKli/XGitFolder.cs
@@ -55,7 +55,7 @@ namespace CKli
                      {
                          Repository.Clone( Url, FileInfo.PhysicalPath, new CloneOptions()
                          {
-                             CredentialsProvider = XGitFolder.ObtainGitCredentialsHandler( m ),
+                             CredentialsProvider = GitFolder.ObtainGitCredentialsHandler( m ),
                          } );
                      }
                      return true;
@@ -75,56 +75,6 @@ namespace CKli
              } );
 
             return true;
-        }
-
-        public static CredentialsHandler ObtainGitCredentialsHandler( IActivityMonitor m )
-        {
-            return ( url, usernameFromUrl, types ) =>
-            {
-                string domain = new Uri( url ).Host;
-                string usernameEnvironmentVariableKey = $"GIT_USER_{domain}";
-                string passwordEnvironmentVariableKey = $"GIT_PWD_{domain}";
-
-                m.Info( $"Looking for credentials for domain {domain} in environment: {usernameEnvironmentVariableKey}, {passwordEnvironmentVariableKey}, and URL" );
-
-                // Guess username: use URL, fallback on env. var
-                string username = usernameFromUrl;
-                if( string.IsNullOrEmpty( username ) )
-                {
-                    username = Environment.GetEnvironmentVariable( usernameEnvironmentVariableKey );
-
-                    if( string.IsNullOrEmpty( username ) )
-                    {
-                        m.Warn( $"Git username was not found at environment variable {usernameEnvironmentVariableKey}. Git might fail if the repository requires authentication." );
-                    }
-                    else
-                    {
-                        m.Info( $"Using Git username from environment variable ({usernameEnvironmentVariableKey}): {username}" );
-                    }
-                }
-                else
-                {
-                    m.Info( $"Using Git username from URL: {username}" );
-                }
-
-                // Read password from env. var
-
-                if( !string.IsNullOrEmpty( username ) )
-                {
-                    string password = Environment.GetEnvironmentVariable( passwordEnvironmentVariableKey );
-
-                    if( string.IsNullOrEmpty( password ) )
-                    {
-                        m.Warn( $"Git password was not found at environment variable {passwordEnvironmentVariableKey}. Git might fail if the repository requires authentication." );
-                    }
-                    else
-                    {
-                        m.Info( $"Using Git password from environment variable ({passwordEnvironmentVariableKey})" );
-                        return new UsernamePasswordCredentials() { Username = username, Password = password };
-                    }
-                }
-                return new DefaultCredentials();
-            };
         }
     }
 }
