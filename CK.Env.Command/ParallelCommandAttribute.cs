cd36d0e5d02e04a671d1a89f32b4c21cf0280042
Kuinox <nicolas.vandeginste@signature-code.com>
2019-07-02 11:33:47 +0200

    WIP

---
+++ CK.Env.Command/ParallelCommandAttribute.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CK.Env
+{
+    [AttributeUsage( AttributeTargets.Method )]
+    public class ParallelCommandAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="always">If true the command is always parallelizable. If false, it should be asked to the user.</param>
+        public ParallelCommandAttribute( bool always = true )
+        {
+            AlwaysRunInParallel = always;
+        }
+        /// <summary>
+        /// Gets if the command should be always parralel. If not it should be asked to the user.
+        /// </summary>
+        public bool AlwaysRunInParallel { get; }
+
+    }
+}
