6f558a960076576ae1a87636c985dc29b2273b30
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-12-05 10:59:48 +0100

    Finalize update.
    Auto detect between stable and prerelease update.
    Removed experimental stdin dotnet run.
    Clarify limitations.

--- CKli.Core/CKliCommands/CKliUpdate.cs
+++ CKli.Core/CKliCommands/CKliUpdate.cs
@@ -17,12 +17,12 @@ sealed class CKliUpdate : Command
                 [],
                 [],
                 [
-                    (["--prerelease"], "Consider prerelease versions (including CI builds)."),
+                    (["--stable"], "Consider stable versions only even if the current version is a prerelease."),
+                    (["--prerelease"], "Consider prerelease versions (including CI builds) even if the current version is stable."),
                     (["--allow-downgrade"], """
                                             Allow package downgrade.
                                             Useful to come back to the last stable version when --prerelease has been used.
-                                            """),
-                    (["--exp"], """!Experimental!""")
+                                            """)
                 ] )
     {
     }
@@ -34,8 +34,13 @@ sealed class CKliUpdate : Command
                                                                     CommandLineArguments cmdLine )
     {
         bool prerelease = cmdLine.EatFlag( "--prerelease" );
+        bool stable = cmdLine.EatFlag( "--stable" );
+        if( prerelease && stable )
+        {
+            monitor.Error( "Flags --prerelease and --stable cannot be both specified." );
+            return ValueTask.FromResult( false );
+        }
         bool allowDowngrade = cmdLine.EatFlag( "--allow-downgrade" );
-        bool useDotnet = cmdLine.EatFlag( "--exp" );
         if( !cmdLine.Close( monitor ) )
         {
             return ValueTask.FromResult( false );
@@ -45,54 +50,40 @@ sealed class CKliUpdate : Command
             monitor.Error( "Update command cannot be used in interactive mode." );
             return ValueTask.FromResult( false );
         }
+        var info = CSemVer.InformationalVersion.ReadFromAssembly( System.Reflection.Assembly.GetExecutingAssembly() );
+        if( !info.IsValidSyntax )
+        {
+            monitor.Error( $"Invalid assembly version: {info.ParseErrorMessage} in '{info.RawVersion}'." );
+            return ValueTask.FromResult( false );
+        }
+        if( !prerelease && !stable )
+        {
+            prerelease = info.Version.IsPrerelease;
+        }
         var updateCmd = prerelease
                 ? "dotnet tool update CKli -g --prerelease --add-source https://pkgs.dev.azure.com/Signature-OpenSource/Feeds/_packaging/NetCore3/nuget/v3/index.json"
                 : "dotnet tool update CKli -g";
         if( allowDowngrade ) updateCmd += " --allow-downgrade";
-        var info = CSemVer.InformationalVersion.ReadFromAssembly( System.Reflection.Assembly.GetExecutingAssembly() );
         monitor.Info( ScreenType.CKliScreenTag, $"""
             Currently installed '{info.Version}'. Will now execute after this CKli instance ends:
             {updateCmd}
             """ );
 
-        // This uses PowerShell and this is not ideal.
-        // IMO, the cmd approach is insane (and not portable).
-        // See https://stackoverflow.com/questions/22558869/wait-for-process-to-end-in-windows-batch-file
-        // I'd rather use a C# 10 file-based application...
-        var pid = System.Environment.ProcessId;
-
-        if( !useDotnet )
+        if( Environment.OSVersion.Platform == PlatformID.Unix )
         {
-            var cmd = $@"Wait-Process -Id {pid} -Timeout 20 -ErrorAction SilentlyContinue; {updateCmd}";
+            monitor.Warn( "Sorry, this is not available on non Windows platform yet." );
+        }
+        else
+        {
+            // This uses windows PowerShell (based on NetFramework 4, the legacy one) should always be available.
+            // The new PowerShell Core (based on .Net) is an opt-in.
+            // FYI, the cmd approach is insane (and not portable anyway).
+            // See https://stackoverflow.com/questions/22558869/wait-for-process-to-end-in-windows-batch-file
+            var cmd = $@"Wait-Process -Id {Environment.ProcessId} -Timeout 20 -ErrorAction SilentlyContinue; {updateCmd};echo ''";
             Process.Start( new ProcessStartInfo
             {
                 FileName = "powershell.exe",
                 Arguments = $"-NoProfile -NoLogo -NonInteractive -ExecutionPolicy unrestricted -command {cmd}",
-                UseShellExecute = false
-            } );
-        }
-        else
-        {
-            var cmd = $$"""
-        'if(System.Diagnostics.Process.GetProcessById({{pid}}).WaitForExit(20000))
-        {System.Diagnostics.Process.Start( "dotnet", "{{updateCmd}}" ).WaitForExit();Console.WriteLine();}' | dotnet run -
-        """.ReplaceLineEndings( "" );
-
-            string fileName, arguments;
-            if( Environment.OSVersion.Platform == PlatformID.Unix )
-            {
-                fileName = "/bin/bash";
-                arguments = "-c " + cmd;
-            }
-            else
-            {
-                fileName = "cmd.exe";
-                arguments = "/c " + cmd;
-            }
-            Process.Start( new ProcessStartInfo
-            {
-                FileName = fileName,
-                Arguments = arguments,
                 UseShellExecute = false
             } );
         }
