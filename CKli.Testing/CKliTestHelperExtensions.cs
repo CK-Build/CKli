1bd318f318f9d9edb566e7d0f9154713da74f287
Olivier Spinelli <olivier.spinelli@signature-code.com>
2026-01-19 10:54:49 +0100

    Setup CKli.Testing.

--- CKli.Testing/CKliTestHelperExtensions.cs
+++ CKli.Testing/CKliTestHelperExtensions.cs
@@ -2,36 +2,41 @@
 using CK.Core;
 using CK.Testing;
 using CKli.Core;
+using LibGit2Sharp;
+using Shouldly;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
+using System.Text;
+using System.Xml.Linq;
 using static CK.Testing.MonitorTestHelper;
 
 namespace CKli;
 
 /// <summary>
-/// Provides extension members to <see cref="IMonitorTestHelper"/>.
+/// Provides extension members to <see cref="IMonitorTestHelper"/> that manage the "Remotes/"
+/// and the "Cloned/" folders. 
 /// <para>
 /// This class has a static initializer that setups the CKli context so it can be used
 /// in NUnit context.
 /// </para>
 /// </summary>
-public static class CKliTestHelperExtensions
+public static partial class CKliTestHelperExtensions
 {
     readonly static NormalizedPath _remotesPath;
     readonly static NormalizedPath _barePath;
     readonly static NormalizedPath _clonedPath;
-    readonly static WorldName _worldName;
+    readonly static WorldName _defaultWorldName;
+    readonly static Dictionary<string, RemotesCollection> _remoteRepositories;
+    readonly static XElement _hostPluginsConfiguration;
 
     static CKliTestHelperExtensions()
     {
-        Throw.CheckState( TestHelper.TestProjectFolder.Path.EndsWith( "/Tests/Plugins.Tests" ) );
-
-        // Preserves the assembly reference to CKli.Plugins.Core.
-        // Without this it is trimmed and the CKli.Plugins.Core is loaded in the plugins context
-        // (even if CKli.Loader itself is pre-loaded).
-        GC.KeepAlive( typeof( CKli.Plugins.PluginCollector ) );
-
         _remotesPath = TestHelper.TestProjectFolder.AppendPart( "Remotes" );
         _barePath = _remotesPath.AppendPart( "bare" );
         _clonedPath = TestHelper.TestProjectFolder.AppendPart( "Cloned" );
@@ -41,23 +46,168 @@ public static class CKliTestHelperExtensions
         int idx = pluginFolderName.IndexOf( "-Plugins" );
         var stackName = pluginFolderName.Substring( 0, idx );
         var ltsName = pluginFolderName.Substring( idx + 8 );
-        _worldName = new WorldName( stackName, ltsName );
+        _defaultWorldName = new WorldName( stackName, ltsName );
 
-        // The CKliRootEnv.AppLocalDataPath is: Environment.SpecialFolder.LocalApplicationData/CKli-Test-StackName-Plugins[LTSName].
-        CKliRootEnv.Initialize( $"Test-{pluginFolderName}",
-                                screen: new StringScreen(),
-                                findCurrentStackPath: false );
-        // Single so that this throws if naming change.
-        var nunitLoadContext = AssemblyLoadContext.All.FirstOrDefault( c => c.GetType().Name == "TestAssemblyLoadContext" );
-        if( nunitLoadContext == null )
+        // We must ensure that the CKli.Plugins.CompiledPlugins is available before loading this assembly
+        // in the load context because once loaded, we won't be able to "update" it if the compiled plugins
+        // weren't available...
+        // We may use a ReflectionOnly context or use the Meta API but this costs. We simply consider that
+        // if the CKli.CompiledPlugins.cs file is present, then it's fine: we are in a test project that
+        // depends on the CKli.Plugins project, so when it is compiled, the CKli.Plugins is also compiled.
+        //
+        // We check that CompileMode is not None and that no plugins are disabled before.
+        // If the user deleted the CKli.CompiledPlugins.cs, he must run "ckli plugin info" to restore the
+        // compiled plugins.
+        //
+        _hostPluginsConfiguration = ReadStackPluginConfiguration( TestHelper.Monitor, _defaultWorldName );
+
+        var ckliPluginsCompiledFile = TestHelper.SolutionFolder.AppendPart( pluginFolderName ).AppendPart( "CKli.Plugins" ).AppendPart( "CKli.CompiledPlugins.cs" );
+        if( !File.Exists( ckliPluginsCompiledFile ) )
         {
-            Throw.InvalidOperationException( """
-                NUnit test adapter doesn't use AssemblyLoadContext named 'TestAssemblyLoadContext'.
-                This must be investigated.
-                """ );
+            Throw.InvalidOperationException( $"The compiled plugins source code generated file is missing: '{ckliPluginsCompiledFile}'." );
         }
-        CKli.Loader.PluginLoadContext.Initialize( nunitLoadContext );
-        World.PluginLoader = CKli.Loader.PluginLoadContext.Load;
+        var runFolder = TestHelper.SolutionFolder.Combine( PluginMachinery.GetLocalRunFolder( pluginFolderName ) );
+        var ckliPluginFilePath = runFolder.AppendPart( "CKli.Plugins.dll" );
+        if( !File.Exists( ckliPluginFilePath ) )
+        {
+            Throw.InvalidOperationException( $"The compiled plugins file is missing: '{ckliPluginFilePath}'." );
+        }
+        var f = GetPluginFactory( ckliPluginFilePath );
+        if( f == null )
+        {
+            Throw.InvalidOperationException( "Unable to get the plugin factory from the compiled plugins." );
+        }
+        World.DirectPluginFactory = f;
+        CKliRootEnv.Initialize( _defaultWorldName.FullName, screen: new StringScreen(), findCurrentStackPath: false );
+
+        _remoteRepositories = InitializeRemotes();
+
+        static XElement ReadStackPluginConfiguration( IActivityMonitor monitor, WorldName worldHostName )
+        {
+            // Reads the host's default world definition definition file <Plugins> element.
+            XElement? stackPlugins = null;
+            try
+            {
+                var stackDefinitionFile = TestHelper.SolutionFolder.AppendPart( $"{worldHostName}.xml" );
+                XDocument stackDefinitionDoc = XDocument.Load( stackDefinitionFile, LoadOptions.PreserveWhitespace );
+                stackPlugins = stackDefinitionDoc.Root?.Element( "Plugins" );
+                if( stackPlugins == null )
+                {
+                    monitor.Error( "The Stack's World '{worldHostName}' has no <Plugins> element." );
+                }
+                else if( stackPlugins.Elements().Any( e => (bool?)e.Attribute( "Disabled" ) is true ) )
+                {
+                    monitor.Error( $"""
+                                    The Stack's World '{worldHostName}' <Plugins> element must no have ANY disabled plugins:
+                                    {stackPlugins}
+                                    """ );
+                    stackPlugins = null;
+                }
+            }
+            catch( Exception ex )
+            {
+                monitor.Error( $"While reading the Stack's World '{worldHostName}' <Plugins> element.", ex );
+                stackPlugins = null;
+            }
+            if( stackPlugins == null )
+            {
+                Throw.InvalidOperationException( $"Unable to read the Stack's World '{worldHostName}' <Plugins> element." );
+            }
+            return stackPlugins;
+        }
+
+        static Func<IPluginFactory>? GetPluginFactory( NormalizedPath ckliPluginFilePath )
+        {
+            var ckliPlugins = Assembly.LoadFrom( ckliPluginFilePath );
+            var compiled = ckliPlugins.GetType( "CKli.Plugins.CompiledPlugins" );
+            var m = compiled?.GetMethod( "UncheckedGet" );
+            return m == null
+                    ? null
+                    : () => (IPluginFactory)m.Invoke( null, [] )!;
+        }
+
+        static Dictionary<string, RemotesCollection> InitializeRemotes()
+        {
+            var remoteIndexPath = _barePath.AppendPart( "Remotes.txt" );
+
+            var zipPath = _remotesPath.AppendPart( "Remotes.zip" );
+            var zipTime = File.GetLastWriteTimeUtc( zipPath );
+            if( !File.Exists( remoteIndexPath )
+                || File.GetLastWriteTimeUtc( remoteIndexPath ) != zipTime )
+            {
+                using( TestHelper.Monitor.OpenInfo( $"Last write time of 'Remotes/' differ from 'Remotes/Remotes.zip'. Restoring remotes from zip." ) )
+                {
+                    RestoreRemotesZipAndCreateBareRepositories( remoteIndexPath, zipPath, zipTime );
+                }
+            }
+            return File.ReadAllLines( remoteIndexPath )
+                        .Select( l => l.Split( '/' ) )
+                        .GroupBy( names => names[0], names => names[1] )
+                        .Select( g => new RemotesCollection( g.Key, g.ToArray() ) )
+                        .ToDictionary( r => r.FullName );
+
+            static void RestoreRemotesZipAndCreateBareRepositories( NormalizedPath remoteIndexPath, NormalizedPath zipPath, DateTime zipTime )
+            {
+                // Cleanup "bare/" content if it exists and delete any existing unzipped repositories.
+                foreach( var stack in Directory.EnumerateDirectories( _remotesPath ) )
+                {
+                    var stackName = Path.GetFileName( stack.AsSpan() );
+                    if( stackName.Equals( "bare", StringComparison.OrdinalIgnoreCase ) )
+                    {
+                        foreach( var openedBare in Directory.EnumerateDirectories( stack ) )
+                        {
+                            FileHelper.DeleteFolder( TestHelper.Monitor, openedBare ).ShouldBeTrue();
+                        }
+                        foreach( var zippedBareOrRemotesIndex in Directory.EnumerateFiles( stack ) )
+                        {
+                            Throw.Assert( Path.GetFileName( zippedBareOrRemotesIndex ) == "Remotes.txt"
+                                          || zippedBareOrRemotesIndex.EndsWith( ".zip" ) );
+                            FileHelper.DeleteFile( TestHelper.Monitor, zippedBareOrRemotesIndex ).ShouldBeTrue();
+                        }
+                    }
+                    else
+                    {
+                        foreach( var repository in Directory.EnumerateDirectories( stack ) )
+                        {
+                            if( !FileHelper.DeleteClonedFolderOnly( TestHelper.Monitor, repository, out var _ ) )
+                            {
+                                TestHelper.Monitor.Warn( $"Folder '{repository}' didn't contain a .git folder. All folders in Remotes/<stack> should be git working folders." );
+                            }
+                        }
+                    }
+                }
+
+                // Extracts Remotes.zip content.
+                // Disallow overwriting: .gitignore file and README.md must not be in the Zip archive.
+                ZipFile.ExtractToDirectory( zipPath, _remotesPath, overwriteFiles: false );
+                // Fills the bare/ with the .zip of the bare repositories and creates the Remotes.txt
+                // index file.
+                var remotesIndex = new StringBuilder();
+                Directory.CreateDirectory( _barePath );
+                foreach( var stack in Directory.EnumerateDirectories( _remotesPath ) )
+                {
+                    var stackName = Path.GetFileName( stack.AsSpan() );
+                    if( !stackName.Equals( "bare", StringComparison.OrdinalIgnoreCase ) )
+                    {
+                        var bareStack = Path.Combine( _barePath, new string( stackName ) );
+                        foreach( var repository in Directory.EnumerateDirectories( stack ) )
+                        {
+                            var src = new DirectoryInfo( Path.Combine( repository, ".git" ) );
+                            var dst = Path.Combine( bareStack, Path.GetFileName( repository ), ".git" );
+                            var target = new DirectoryInfo( dst );
+                            FileUtil.CopyDirectory( src, target );
+                            using var r = new Repository( dst );
+                            r.Config.Set( "core.bare", true );
+                            remotesIndex.AppendLine( $"{stackName}/{Path.GetFileName( repository )}" );
+                        }
+                        ZipFile.CreateFromDirectory( bareStack, bareStack + ".zip" );
+                    }
+                }
+                File.WriteAllText( remoteIndexPath, remotesIndex.ToString() );
+                File.SetLastWriteTimeUtc( remoteIndexPath, zipTime );
+            }
+        }
+
     }
 
     extension( IMonitorTestHelper helper )
@@ -70,19 +220,78 @@ public static class CKliTestHelperExtensions
 
         /// <summary>
         /// Gets the "Cloned/" path in the test project folder where
-        /// remote repositories are cloned and used by tests.
+        /// remote repositories are cloned and used by tests (see <see cref="RemotesCollection.Clone"/>).
         /// </summary>
         public NormalizedPath CKliClonedPath => _clonedPath;
 
         /// <summary>
-        /// Gets the World name that contains the plugins (and these tests).
+        /// Gets the default World name that contains the plugins (and these tests).
         /// </summary>
-        public WorldName CKliWorldName => _worldName;
+        public WorldName CKliDefaultWorldName => _defaultWorldName;
 
         /// <summary>
         /// Gets the <see cref="StackRepository.StackWorkingFolder"/> (the ".PrivateStack" or ".PublicStack" folder)
         /// of the host stack.
         /// </summary>
         public NormalizedPath CKliStackWorkingFolder => helper.SolutionFolder;
+
+        /// <summary>
+        /// Must be called by tests to cleanup their respective "Cloned/&lt;test-name&gt;" where they can clone
+        /// the stacks they want from the "Remotes" thanks to <see cref="RemotesCollection.Clone(NormalizedPath, Action{IActivityMonitor, XElement}?)"/>.
+        /// </summary>
+        /// <param name="methodTestName">The test name.</param>
+        /// <param name="clearStackRegistryFile">True to clear the stack registry (<see cref="StackRepository.ClearRegistry"/>).</param>
+        /// <returns>The path to the cleaned folder.</returns>
+        public NormalizedPath InitializeClonedFolder( [CallerMemberName] string? methodTestName = null, bool clearStackRegistryFile = true )
+        {
+            var path = _clonedPath.AppendPart( methodTestName );
+            if( Directory.Exists( path ) )
+            {
+                RemoveAllReadOnlyAttribute( path );
+                helper.CleanupFolder( path, ensureFolderAvailable: true );
+            }
+            else
+            {
+                Directory.CreateDirectory( path );
+            }
+            if( clearStackRegistryFile )
+            {
+                Throw.CheckState( StackRepository.ClearRegistry( TestHelper.Monitor ) );
+            }
+            return path;
+
+            static void RemoveAllReadOnlyAttribute( string folder )
+            {
+                var options = new EnumerationOptions
+                {
+                    IgnoreInaccessible = false,
+                    RecurseSubdirectories = true,
+                    AttributesToSkip = FileAttributes.System
+                };
+                foreach( var f in Directory.EnumerateFiles( folder, "*", options ) )
+                {
+                    File.SetAttributes( f, FileAttributes.Normal );
+                }
+            }
+        }
+
+        /// <summary>
+        /// Obtains a clean (unmodified) <see cref="RemotesCollection"/> that must exist.
+        /// </summary>
+        /// <param name="fullName">The <see cref="RemotesCollection.FullName"/> to use.</param>
+        /// <returns>The remotes collection.</returns>
+        public static RemotesCollection OpenRemotes( string fullName )
+        {
+            Throw.DebugAssert( _remoteRepositories != null );
+            var r = _remoteRepositories[fullName];
+            // Deletes the current repository that may have been modified
+            // and extracts a brand new bare git repository.
+            var path = _barePath.AppendPart( r.FullName );
+            FileHelper.DeleteFolder( TestHelper.Monitor, path ).ShouldBeTrue();
+            ZipFile.ExtractToDirectory( path + ".zip", path, overwriteFiles: false );
+            return r;
+        }
+
+
     }
 }
