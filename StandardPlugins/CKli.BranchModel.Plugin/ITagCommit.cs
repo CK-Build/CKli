b87357f5ca68c1611097d3860d1daf4108893c17
Olivier Spinelli <olivier.spinelli@signature-code.com>
2026-07-20 13:51:29 +0200

    Shared default ITagCommit implementation.

--- StandardPlugins/CKli.BranchModel.Plugin/ITagCommit.cs
+++ StandardPlugins/CKli.BranchModel.Plugin/ITagCommit.cs
@@ -1,6 +1,7 @@
 using CK.Core;
 using CKli.Core;
 using LibGit2Sharp;
+using System;
 
 namespace CKli.BranchModel.Plugin;
 
@@ -24,4 +25,32 @@ public interface ITagCommit
     /// Gets the commit.
     /// </summary>
     Commit Commit { get; }
+
+
+    /// <summary>
+    /// Creates a minimal <see cref="ITagCommit"/> instance.
+    /// </summary>
+    /// <param name="repo">The repository.</param>
+    /// <param name="version">The version.</param>
+    /// <param name="commit">The commit.</param>
+    /// <returns></returns>
+    public static ITagCommit Create( Repo repo, SVersion version, Commit commit )
+    {
+        return new TagCommit( repo, version, commit );
+    }
+
+    sealed class TagCommit : ITagCommit
+    {
+        public TagCommit( Repo repo, SVersion version, Commit commit )
+        {
+            Repo = repo;
+            Version = version;
+            Commit = commit;
+        }
+
+        public Repo Repo { get; }
+        public SVersion Version { get; }
+        public Commit Commit { get; }
+        public override string ToString() => $"Tag '{Repo.DisplayPath}/{Version.ParsedText}' references Commit '{Commit.Sha}'";
+    }
 }
